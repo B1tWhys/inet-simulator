f36d55feb9c7451dc7d1afa87793c5584f33c4ae
skyler arnold <skyarnold1@me.com>
2021-10-28 19:16:45 -0600

    new as

--- inet_manager/inet_manager/cli/commands.py
+++ inet_manager/inet_manager/cli/commands.py
@@ -10,14 +10,9 @@ pass_inet = click.make_pass_decorator(Internet, ensure=True)
 
 @click.group()
 @click.pass_context
-def root(ctx):
+def root(ctx: click.Context):
     ctx.obj = select_internet()
-
-
-@root.command("save")
-@pass_inet
-def save(inet):
-    storage.save_inet(inet)
+    ctx.call_on_close(lambda: storage.save_inet(ctx.obj))
 
 
 @root.group("docker")
@@ -35,19 +30,27 @@ def new():
     pass
 
 
-@new.command("internet")
-def new_internet():
-    raise NotImplementedError()
+@new.command("as")
+@pass_inet
+def new_as(inet: Internet):
+    existing_names = {a.name for a in inet.list_autonomous_systems()}
+    default_name = f"as-{inet.next_asn()}"
+    name = prompt_for_new_name("enter name for new AS: ", existing_names, default=default_name)
+    inet.create_as(name)
 
 
 @new.command("server")
-def new_server():
-    raise NotImplementedError()
+@pass_inet
+def new_server(inet: Internet):
+    as_ = select_as(inet, 'select as to create the server in: ')
+    if as_ is None:
+        print("You need to create an AS first")
+        return
 
-
-@new.command("as")
-def new_server():
-    raise NotImplementedError()
+    current_names = [s.name for s in as_.list_servers()]
+    default = gen_default_name(f'server{as_.asn}-', current_names)
+    name = prompt_for_new_name("enter name for new server: ", existing_names=current_names, default=default)
+    as_.create_server(name)
 
 
 @new.command("router")
@@ -85,8 +88,8 @@ def ls_servers():
     raise NotImplementedError()
 
 
-def prompt_for_new_name(message, existing_names, default=""):
-    return "placeholder name"
+def prompt_for_new_name(message, existing_names, default=None):
+    return inquirer.text(message=message, validate=lambda c, m: m not in existing_names, default=default)
 
 
 def gen_default_name(prefix, existing_names):
@@ -94,6 +97,13 @@ def gen_default_name(prefix, existing_names):
         name = prefix + str(i)
         if name not in existing_names:
             return name
+
+
+def create_internet():
+    existing_names = storage.get_saved_inet_names()
+    default_name = gen_default_name('inet-', existing_names)
+    name = prompt_for_new_name("enter name for new internet", existing_names, default=default_name)
+    return Internet(name=name)
 
 
 def select_internet():
@@ -109,8 +119,13 @@ def select_internet():
         return storage.load_inet(answer['inet_name'])
 
 
-def create_internet():
-    existing_names = storage.get_saved_inet_names()
-    default_name = gen_default_name('inet-', existing_names)
-    name = prompt_for_new_name(f"enter name for new internet: ", existing_names, default=default_name)
-    return Internet(name=name)
+def select_as(inet, message):
+    choices = [(a.name, a) for a in inet.list_autonomous_systems()]
+    if len(choices) == 0:
+        return None
+    elif len(choices) == 1:
+        print("only one AS exists. I assume you want that one")
+        return choices[0][1]
+    answer = inquirer.prompt([inquirer.List('as', message=message, choices=choices)])
+    as_ = answer['as']
+    return as_
